79da9205f67bcfce165594a99762b6f3f973c752
obt <byeongtaek12@naver.com>
2025-02-28 12:08:25 +0900

    초기 ì코드

---
+++ src/Calculator.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Calculator {
+    List<Long> resultSave = new ArrayList<>();
+
+    public Long calculate (long x, char oper, long y) {
+        int result = x;
+        return result;
+    }
+}
