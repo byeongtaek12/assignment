4ec310661118c51c690576de487450d2a453fde0
obt <byeongtaek12@naver.com>
2025-02-28 15:43:12 +0900

    중간저장

--- src/App.java
+++ src/App.java
@@ -1,6 +1,58 @@
 
+import java.util.InputMismatchException;
+import java.util.List;
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) {
+        Calculator cal = new Calculator();
+        Scanner scanner = new Scanner(System.in);
+        boolean exit = true;
+        while (exit) {
+            long x = 0;
+            long y = 0;
+            Object result;
+            while (true) {
+                try {
+                    System.out.print("첫 번째 양의 정수(0 포함)를 입력해주세요: ");
+                    x = scanner.nextLong();
+                    System.out.print("두 번째 양의 정수(0 포함)를 입력해주세요: ");
+                    y = scanner.nextLong();
+                    if (x < 0 || y < 0) {
+                        System.out.println("음수를 적으셨습니다. 다시 입력해주세요.");
+                        continue;
+                    }
 
+                } catch (InputMismatchException e) {
+                    scanner.nextLine();
+                    System.out.println("숫자만 입력해주세요");
+                    continue;
+                }
+                while (true) {
+                    System.out.print("사칙연산 기호를 입력하세요: ");
+                    char oper = scanner.next().charAt(0);
+                    result = Calculator.calculate(x, oper, y);
+                    if (result.equals('x')) {
+                        continue;
+                    }
+                    System.out.println("계산 값: " + result);
+                    break;
+                }
+                System.out.println("값을 저장소에 저장합니다");
+                List<Object> newResultSave = cal.getResultSave();
+                newResultSave.add(result);
+                cal.setResultSave(newResultSave);
+                break;
+            }
+
+            scanner.nextLine();
+            System.out.println("더 계산하시겠습니까? (exit 입력시 종료, 다른 입력시 계속)");
+            String s = scanner.nextLine();
+            if (s.equals("exit")) {
+                exit = false;
+                System.out.println("종료하겠습니다");
+            }
+        }
     }
+
 }
--- src/Calculator.java
+++ src/Calculator.java
@@ -2,25 +2,37 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Calculator {
-    List<Object> resultSave = new ArrayList<>();
+    private List<Object> resultSave = new ArrayList<>();
 
-    public Object calculate (long x, char oper, long y) {
+    public static Object calculate(long x, char oper, long y) {
         Object result = switch (oper) {
-            case '+' -> x + y ;
-            case '-' -> x - y ;
-            case '*' -> x * y ;
+            case '+' -> x + y;
+            case '-' -> x - y;
+            case '*' -> x * y;
             case '/' -> {
                 if (y == 0) {
-                    System.out.println("0으로 나눌 수 없으므로 0을 반환합니다");
-                    yield 0;
+                    System.out.println("0으로 나눌 수 없으므로 x를 반환합니다");
+                    yield 'x';
                 }
-                yield x / (double)y;
+                yield x / (double) y;
             }
             default -> {
-                System.out.println("연산기호가 잘못됐으므로 0을 반환합니다");
-                yield 0;
+                System.out.println("연산기호가 잘못됐으므로 x를 반환합니다");
+                yield 'x';
             }
         };
         return result;
+    }
+
+    public List<Object> getResultSave() {
+        return this.resultSave;
+    }
+
+    public void setResultSave(List<Object> resultSave) {
+        this.resultSave=resultSave;
+    }
+
+    public void removeResult() {
+
     }
 }
