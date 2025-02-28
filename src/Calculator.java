4e85d2a7740c5378ca8fe86062e1c39685daf1c6
obt <byeongtaek12@naver.com>
2025-02-28 12:47:15 +0900

    초ê¸Calculator완성

--- src/Calculator.java
+++ src/Calculator.java
@@ -2,10 +2,25 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Calculator {
-    List<Long> resultSave = new ArrayList<>();
+    List<Object> resultSave = new ArrayList<>();
 
-    public Long calculate (long x, char oper, long y) {
-        int result = x;
+    public Object calculate (long x, char oper, long y) {
+        Object result = switch (oper) {
+            case '+' -> x + y ;
+            case '-' -> x - y ;
+            case '*' -> x * y ;
+            case '/' -> {
+                if (y == 0) {
+                    System.out.println("0으로 나눌 수 없으므로 0을 반환합니다");
+                    yield 0;
+                }
+                yield x / (double)y;
+            }
+            default -> {
+                System.out.println("연산기호가 잘못됐으므로 0을 반환합니다");
+                yield 0;
+            }
+        };
         return result;
     }
 }
